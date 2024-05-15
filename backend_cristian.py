8155bc3333510d270d2e1ebe3cf2ad3a47f22eb1
Criscv <143120018+Criscv@users.noreply.github.com>
2024-05-14 21:16:17 -0600

    Ultima ayuda al back

---
+++ backend_cristian.py
@@ -0,0 +1,118 @@
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, ConversationalRetrievalChain, ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import messages_from_dict, messages_to_dict
+from langchain.memory.chat_message_histories.in_memory import ChatMessageHistory
+from langchain.agents import Tool
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+import pandas as pd
+from langchain.document_loaders import GutenbergLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+import tempfile
+from langchain.llms import HuggingFacePipeline
+from langchain.chains import RetrievalQA
+
+cache_dir = "/content/"
+
+pd.set_option('display.max_column', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_seq_items', None)
+pd.set_option('display.max_colwidth', 500)
+pd.set_option('expand_frame_repr', True)
+
+# Obtener la informacion
+loader = GutenbergLoader(
+    "https://www.gutenberg.org/cache/epub/1268/pg1268.txt"
+)
+
+# Cargar informacion en memoria
+document = loader.load()
+
+extrait = ' '.join(document[0].page_content.split()[:100])
+display(extrait + " .......")
+
+
+# Dividir o cortar el texto
+# Chunk sizes of 1024 and an overlap of 256 (this will take approx. 10mins with this model to build our vector database index)
+text_splitter = CharacterTextSplitter(
+    chunk_size=1024,
+    chunk_overlap=256
+)
+texts = text_splitter.split_documents(document)
+
+model_name = "sentence-transformers/all-MiniLM-L6-v2"
+
+# Guardar la informacion codificada en embeddings
+embeddings = HuggingFaceEmbeddings(
+    model_name=model_name,
+    cache_folder=cache_dir # Modelo preentrenado
+)  # Use a pre-cached model
+
+#Guarda la informacion en chroma
+vectordb = Chroma.from_documents(
+    texts,
+    embeddings,
+    persist_directory=cache_dir
+)
+
+
+# We want to make this a retriever, so we need to convert our index.
+# This will create a wrapper around the functionality of our vector database
+# so we can search for similar documents/chunks in the vectorstore and retrieve the results:
+retriever = vectordb.as_retriever()
+
+# This chain will be used to do QA on the document. We will need
+# 1 - A LLM to do the language interpretation
+# 2 - A vector database that can perform document retrieval
+# 3 - Specification on how to deal with this data
+
+hf_llm = HuggingFacePipeline.from_model_id(
+    model_id="google/flan-t5-large",
+    task="text2text-generation",
+    model_kwargs={
+#        "temperature": 0,
+        "do_sample":True,
+        "max_length": 2048,
+        "cache_dir": cache_dir,
+    },
+)
+
+# Backend David
+'''
+qa = RetrievalQA.from_chain_type(
+    llm=hf_llm,
+    chain_type="refine",
+    retriever=retriever
+)
+query = "Who is the main character?"
+query_results_venice = qa.run(query)
+print("#" * 12)
+query_results_venice'''
+
+# Implementacion Cristian
+
+while True:
+    # Crear una instancia de RetrievalQA
+    qa = RetrievalQA.from_chain_type(
+        llm=hf_llm,
+        chain_type="refine",
+        retriever=retriever
+    )
+
+    # Pedir al usuario la consulta
+    query = input("Ingrese su pregunta (o escriba 'salir' para terminar): ")
+
+    # Verificar si el usuario quiere salir del programa
+    if query.lower() == "salir":
+        print("Saliendo del programa...")
+        break
+
+    # Ejecutar la consulta
+    query_results_venice = qa.run(query)
+
+    # Imprimir los resultados
+    print("#" * 12)
+    print(query_results_venice)
